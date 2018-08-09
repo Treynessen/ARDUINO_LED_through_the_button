8f87fc663453e5660fdfb58a1cc823f6c3453c9a
tranerius <36367029+tranerius@users.noreply.github.com>
2018-08-09 14:44:47 +0200

    add

--- sketch_aug05a/sketch_aug05a.ino
+++ sketch_aug05a/sketch_aug05a.ino
@@ -10,6 +10,7 @@ byte count_low_signal = 0;
 void loop() {
   // Защита от дребезга контактов. Смена состояния сигнала, если количество низких сигналов = 100
   if (!low_signal && digitalRead(5) && count_low_signal++ >= 100) low_signal = true; 
+  else if (!low_signal && !digitalRead(5)) count_low_signal = 0;
   // Включение/выключение диода происходит в том случае, если предыдущий сигнал был низкий, что соответствует не нажатой кнопке
   if (low_signal && !digitalRead(5)) {
     low_signal = false;
